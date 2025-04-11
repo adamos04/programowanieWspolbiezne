ba8d41531c44025bf85302040f2d29554a7aff3e
251669 Ziółkowski Adam <251669@edu.p.lodz.pl>
2025-04-11 08:46:15 +0200

    change main window

--- GraphicalUserInterface/MainWindow.xaml.cs
+++ GraphicalUserInterface/MainWindow.xaml.cs
@@ -22,17 +22,17 @@ namespace TP.ConcurrentProgramming.PresentationView
     {
       Random random = new Random();
       InitializeComponent();
-      MainWindowViewModel viewModel = (MainWindowViewModel)DataContext;
-      double screenWidth = SystemParameters.PrimaryScreenWidth;
-      double screenHeight = SystemParameters.PrimaryScreenHeight;
-      viewModel.Start(random.Next(5, 10));
-    }
+            MainWindowViewModel viewModel = (MainWindowViewModel)DataContext;
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+            viewModel.Start(random.Next(5, 10));
+        }
 
-    /// <summary>
-    /// Raises the <seealso cref="System.Windows.Window.Closed"/> event.
-    /// </summary>
-    /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
-    protected override void OnClosed(EventArgs e)
+        /// <summary>
+        /// Raises the <seealso cref="System.Windows.Window.Closed"/> event.
+        /// </summary>
+        /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
+        protected override void OnClosed(EventArgs e)
     {
       if (DataContext is MainWindowViewModel viewModel)
         viewModel.Dispose();
