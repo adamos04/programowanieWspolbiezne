a7ea7cad4d926440801da8823fc7c8c10cb102ae
EmelusP <pierzchalaemil@gmail.com>
2025-04-27 17:28:23 +0200

    Moving collision point from top left corner to the center

--- PresentationModel/ModelBall.cs
+++ PresentationModel/ModelBall.cs
@@ -69,8 +69,9 @@ namespace TP.ConcurrentProgramming.Presentation.Model
 
     private void NewPositionNotification(object sender, IPosition e)
     {
-      Top = e.y; Left = e.x;
-    }
+            Top = e.y - Diameter / 2; 
+            Left = e.x - Diameter / 2;
+        }
 
     private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
     {
